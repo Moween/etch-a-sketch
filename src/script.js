452f19a99a9e5c978d5f9cb7ea12aa3337bda897
moween <moween1998@gmail.com>
2021-07-03 10:57:48 +0100

    Change square background-color.

--- src/script.js
+++ src/script.js
@@ -11,22 +11,26 @@ class Square {
 
   handleColorEffect(e) {
     e.preventDefault();
-    e.target.style.backgroundColor = 'red';
+    const { target: square } = e;
+    const green = Math.floor(Math.random() * (256 - 0));
+    const red = Math.floor(Math.random() * (256 - 0));
+    const blue = Math.floor(Math.random() * (256 - 0));
+    square.style.backgroundColor = `rgb(${green}, ${red}, ${blue})`;
   }
 }
 
 const renderSquare = () => {
-  for (let i = 0; i < 200; i++) {
+  for (let i = 0; i < 200; i += 1) {
     const { divElem } = new Square();
     container.append(divElem);
-  }  
-}
+  }
+};
 
 const handleClearSketch = (e) => {
   e.preventDefault();
   container.innerHTML = '';
   renderSquare();
-}
+};
 
 renderSquare();
 clearBtn.addEventListener('click', handleClearSketch);
